faab528437ab59b2d99a2d763736acacdc367fbd
Enoch Martinez <enoch@silverclickmedia.com>
2026-03-11 15:10:14 +0900

    Intial MemoryMadness project

---
+++ game_over/game_over.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+@onready var moves_label = $NinePatchRect/MC/VB/HB/MovesLabel
+
+
+
+func _ready():
+	SignalManager.on_game_over.connect(on_game_over) 
+
+
+func on_game_over(moves: int) ->void:
+	show()
+	moves_label.text = str(moves)
+
+
+
+func _on_exit_button_pressed():
+	hide()
+	SignalManager.on_game_exit_pressed.emit()
